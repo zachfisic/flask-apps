296648a2d19dfa028e464a1d0c3d6375a3c1db13
Zach Fisic <zachfisic@gmail.com>
2018-04-20 18:29:19 -0700

    Add control structures to microblog

--- tutorials/microblog/app/routes.py
+++ tutorials/microblog/app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
   user = {'username': 'Zach'}
-  return render_template('index.html', title='Home', user=user)
+  posts = [
+    {
+      "author": {"username": "John"},
+      "body": "A simple test post"
+    },
+    {
+      "author": {"username": "Peter"},
+      "body": "Flask is awesome"
+    },
+  ]
+  return render_template('index.html', title='Home', user=user, posts=posts)
