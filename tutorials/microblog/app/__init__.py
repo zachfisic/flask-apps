f4d0be3573f826411e96ef97e98733c7654a4114
Zach Fisic <zachfisic@gmail.com>
2018-04-26 10:14:03 -0700

    Add DB modeling and migration func to microblog

--- tutorials/microblog/app/__init__.py
+++ tutorials/microblog/app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # import routes at bottom to prevent circular module importing
-from app import routes
+from app import routes, models
