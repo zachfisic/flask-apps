99603003ba085b656d1dc5154ac4eff84784902d
Zach Fisic <zachfisic@gmail.com>
2018-04-26 11:20:20 -0700

    Login functionality

--- tutorials/microblog/app/__init__.py
+++ tutorials/microblog/app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 # import routes at bottom to prevent circular module importing
 from app import routes, models
