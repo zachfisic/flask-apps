e548d50973b5641541843168caf79809ef894e14
Zach Fisic <zachfisic@gmail.com>
2018-04-27 09:09:18 -0700

    Handle login requirements

--- tutorials/microblog/app/__init__.py
+++ tutorials/microblog/app/__init__.py
@@ -9,6 +9,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+login.login_view = 'login'
 
 # import routes at bottom to prevent circular module importing
 from app import routes, models
