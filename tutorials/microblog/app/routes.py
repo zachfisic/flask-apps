06a970cdd1b2ece5aa3e53d443951b9ed9bd452b
Zach Fisic <zachfisic@gmail.com>
2018-04-27 18:23:54 -0700

    Add profile pages to microblog

--- tutorials/microblog/app/routes.py
+++ tutorials/microblog/app/routes.py
@@ -59,3 +59,14 @@ def register():
     flash('You have been registered.')
     return redirect(url_for('login'))
   return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+  user = User.query.filter_by(username=username).first_or_404()
+  posts = [
+    { "author": user, "body": "Test Post No.1" },
+    { "author": user, "body": "Test Post No.2" }
+  ]
+  return render_template('user.html', user=user, posts=posts)
