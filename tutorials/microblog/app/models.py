9ac290e79fefbdd3eb30930c0697978f5a6f449f
Zach Fisic <zachfisic@gmail.com>
2018-04-30 15:52:02 -0700

    Use gravatar for profile avatars

--- tutorials/microblog/app/models.py
+++ tutorials/microblog/app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import db, login
@@ -18,6 +19,10 @@ class User(UserMixin, db.Model):
   
   def check_password(self, password):
     return check_password_hash(self.password_hash, password)
+
+  def avatar(self, size):
+    digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+    return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
 
 class Post(db.Model):
