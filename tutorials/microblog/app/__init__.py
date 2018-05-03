43fa31e750f847fe3145eeaa1130a1f8b56fc4a3
Zach Fisic <zachfisic@gmail.com>
2018-05-03 15:24:49 -0700

    Log errors to rotating files

--- tutorials/microblog/app/__init__.py
+++ tutorials/microblog/app/__init__.py
@@ -1,5 +1,5 @@
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -31,6 +31,15 @@ if not app.debug:
     )
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
+
+  if not os.path.exists('logs'):
+    os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
 # import routes at bottom to prevent circular module importing
 from app import routes, models, errors
