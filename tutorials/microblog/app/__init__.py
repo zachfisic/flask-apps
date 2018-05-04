c35856b830e1973c3b144087523e3eac8006b511
Zach Fisic <zachfisic@gmail.com>
2018-05-03 17:10:34 -0700

    Implement follower logic

--- tutorials/microblog/app/__init__.py
+++ tutorials/microblog/app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import os
 from config import Config
 
 app = Flask(__name__)
@@ -14,23 +15,23 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 if not app.debug:
-  if app.config['MAIL_SERVER']:
-    auth = None
-    if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-      auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-    secure = None
-    if app.config['MAIL_USE_TLS']:
-      secure = ()
-    mail_handler = SMTPHandler(
-      mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-      fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-      toaddrs=app.config['ADMINS'],
-      subject='Microblog failure',
-      credentials=auth,
-      secure=secure
-    )
-    mail_handler.setLevel(logging.ERROR)
-    app.logger.addHandler(mail_handler)
+  # if app.config['MAIL_SERVER']:
+  #   auth = None
+  #   if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+  #     auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+  #   secure = None
+  #   if app.config['MAIL_USE_TLS']:
+  #     secure = ()
+  #   mail_handler = SMTPHandler(
+  #     mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+  #     fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+  #     toaddrs=app.config['ADMINS'],
+  #     subject='Microblog failure',
+  #     credentials=auth,
+  #     secure=secure
+  #   )
+  #   mail_handler.setLevel(logging.ERROR)
+  #   app.logger.addHandler(mail_handler)
 
   if not os.path.exists('logs'):
     os.mkdir('logs')
