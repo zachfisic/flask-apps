2b4d211c0f5be2357456308cd2c8a6ebdb4361db
Zach Fisic <zachfisic@gmail.com>
2018-05-10 11:30:33 -0700

    Paginate posts on user page

--- tutorials/microblog/app/routes.py
+++ tutorials/microblog/app/routes.py
@@ -87,11 +87,12 @@ def register():
 def user(username):
   """Display user given by route param"""
   user = User.query.filter_by(username=username).first_or_404()
-  posts = [
-    { "author": user, "body": "Test Post No.1" },
-    { "author": user, "body": "Test Post No.2" }
-  ]
-  return render_template('user.html', user=user, posts=posts)
+  page = request.args.get('page', 1, type=int)
+  posts = user.posts.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+  next_url = url_for('user', username=user.username, page=posts.next_num) if posts.has_next else None
+  prev_url = url_for('user', username=user.username, page=posts.prev_num) if posts.has_prev else None
+
+  return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 
 
